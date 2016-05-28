bad503f761a45b8473f1cc7d855ee69158d961d8
javenkn <javenkn7@yahoo.com>
2016-05-28 10:07:05 -1000

    passed all tests

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,23 +7,39 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
-
+  var prevNumber = 0;
+  var currNumber = 1;
+  var nextNumber = 1;
   // do your work here
-
+  while(prevNumber <= maxFibValue){
+    if(prevNumber % 2 === 0){
+      sum = sum + prevNumber;
+    }
+    prevNumber = currNumber;
+    currNumber = nextNumber;
+    nextNumber = currNumber + prevNumber;
+  }
+  console.log(sum);
   return sum;
 }
-
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
-
+  var prevNumber = 0;
+  var currNumber = 1;
+  var nextNumber = 1;
   //define your base case, validate your input
 
 
   //do your work here
-
+  while(prevNumber < maxFibValue){
+      highest = prevNumber;
+      prevNumber = currNumber;
+      currNumber = nextNumber;
+      nextNumber = currNumber + prevNumber;
+  }
   return highest;
-};
+}
 
 /**
  * Do not modify code below.
